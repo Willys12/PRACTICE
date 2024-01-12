4b057a5b2439604ff2ede04a271ad4efff069255
Willys12 <ben.willys92@gmail.com>
2024-01-12 17:29:54 +0300

    Static Method: new file created

---
+++ static.py
@@ -0,0 +1,23 @@
+class Item:
+    #Placeholder for Item class
+    pass
+
+class Phone(Item):
+    all = []
+    def __init__(self, name: str, price: float, quantity=0, broken_phones=0):
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+        assert broken_phones >= 0, f"Broken phones {broken_phones} is not greater than or equal to zero!"
+        
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.broken_phones = broken_phones
+        
+        #Action to execute
+        Phone.all.append(self)
+        
+Phone1 = Phone("jseriesv1", 50000, 5, 1)
+Phone2 = Phone("jseriesv2", 68000, 6, 1)
+
+print(Phone.all)
