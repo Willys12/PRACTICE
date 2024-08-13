89f7d5b2b336485a5144bc843c3367a6d99a48e8
Willys12 <ben.willys92@gmail.com>
2024-08-13 07:51:13 +0300

    Practice: Arrays in JS

--- JSFiles/fill.js
+++ JSFiles/fill.js
@@ -1,7 +1,18 @@
 // Append item to an array.
 const fruits = ['banana', 'mango', 'apple', 'strawberry', 'kiwi', 'guava', 'melon'];
-const start = 0;
-const deleteCount = 3;
+const start = -2;
+const deleteCount = 2;
 
-const removeItems = fruits.splice(start, deleteCount);
+const removeItems = fruits.splice(start, deleteCount, 'bitrut', 'lemon');
+console.log(fruits);
+
+// separator
+console.log('***********SEPARATOR**************');
 console.log(removeItems);
+
+// sep 2
+console.log('***********SEPARATOR**************');
+
+for (const fruit of fruits) {
+    console.log(fruit);
+}
